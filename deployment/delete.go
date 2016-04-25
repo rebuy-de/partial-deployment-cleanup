ec35a8da3b6b8deef68318891fb31159105e8c92
Sven Walter <s.walter@rebuy.de>
2016-04-25 16:24:54 +0200

    add deletion of deployment directories

---
+++ deployment/delete.go
@@ -0,0 +1,40 @@
+package deployment
+
+import (
+	"flag"
+	"log"
+	"os"
+	"path"
+)
+
+var (
+	deploymentPath = flag.String("deployment-path", "/opt/www", "")
+)
+
+func Delete(project, branch string) {
+	directory := getPath(project, branch)
+
+	for _, sub := range []string{"current", "release", "shared"} {
+		if !isDirectory(path.Join(directory, sub)) {
+			log.Printf(`Aborting deletion of directory '%s', `+
+				`because it doesn't look like a deployment directory. `+
+				`The subdirectory '%s' is missing.`, directory, sub)
+			return
+		}
+	}
+
+	os.RemoveAll(directory)
+}
+
+func getPath(project, branch string) string {
+	return path.Join(*deploymentPath, project, branch)
+}
+
+func isDirectory(path string) bool {
+	fileInfo, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
+
+	return fileInfo.IsDir()
+}
---
+++ deployment/delete_test.go
@@ -0,0 +1,63 @@
+package deployment
+
+import (
+	"io/ioutil"
+	"os"
+	"path"
+	"testing"
+)
+
+func testHelperTempDir(t *testing.T, project string, branch string) (string, func()) {
+	tmp, err := ioutil.TempDir("", "pdtest")
+	if err != nil {
+		t.Logf(err.Error())
+		t.FailNow()
+	}
+
+	os.MkdirAll(path.Join(tmp, project, branch), 0755)
+
+	return tmp, func() {
+		os.RemoveAll(tmp)
+	}
+}
+
+func testHelperDeploymentDirectory(t *testing.T, project string, branch string) (string, func()) {
+	tmp, fn := testHelperTempDir(t, project, branch)
+
+	for _, sub := range []string{"current", "release", "shared"} {
+		os.Mkdir(path.Join(tmp, project, branch, sub), 0755)
+	}
+
+	return tmp, fn
+}
+
+func TestDeploymentDelete(t *testing.T) {
+	tmp, fn := testHelperDeploymentDirectory(t, "proj", "master")
+	defer fn()
+
+	deploymentPath = &tmp
+	Delete("proj", "master")
+
+	dir := path.Join(tmp, "proj", "master")
+	if isDirectory(dir) {
+		t.Logf("`Delete` should have deleted '%s', but didn't.", dir)
+		t.Fail()
+	}
+}
+
+func TestDeploymentDeleteSkip(t *testing.T) {
+	for _, sub := range []string{"current", "release", "shared"} {
+		tmp, fn := testHelperDeploymentDirectory(t, "proj", "master")
+		defer fn()
+
+		os.Remove(path.Join(tmp, "proj", "master", sub))
+		deploymentPath = &tmp
+		Delete("proj", "master")
+
+		dir := path.Join(tmp, "proj", "master")
+		if !isDirectory(dir) {
+			t.Logf("`Delete` shouldn't have deleted '%s', but did.", dir)
+			t.Fail()
+		}
+	}
+}
